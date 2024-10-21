8b6ce08d87eb62c7960bb5d8c0ba7a7b6b0b710b
lenovo <omkar.reddy211@gmailcom>
2024-10-21 16:19:03 +0530

    payment script

--- makefile
+++
@@ -1,3 +0,0 @@
-default:
-	@git pull &>/dev/null
-	ansible-playbook -i $(app_name)-$(env).rdevopsb72.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$(env) -e app_name=$(app_name) roboshop.yml
--- run.sh
+++
@@ -1,7 +0,0 @@
-git pull
-if [ $# -ne 2 ]; then
-  echo "Input Missing"
-  echo "Usage: bash run.sh <component-name> <env>"
-  exit 1
-fi
-ansible-playbook -i $1-$2.rdevopsb72.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e app_name=$1 roboshop.yml
