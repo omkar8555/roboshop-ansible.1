7e2b3a25528c4b546ee2357d991561f5c0916fd8
lenovo <omkar.reddy211@gmailcom>
2024-11-13 14:31:04 +0530

    payment script

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ if [ $# -ne 2 ]; then
   echo "Usage: bash run.sh <component-name> <env>"
   exit 1
 fi
-ansible-playbook -i $1-$2.rdevopsb72.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e app_na
+ansible-playbook -i $1-$2.rdevopsb72.shop, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev component=$1 roboshop.yml
